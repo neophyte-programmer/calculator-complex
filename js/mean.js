f275321f830349b2d23764530ef35e2257a3161b
Nutifafa Attor <attorfafa@gmail.com>
2022-01-15 17:51:52 +0000

    mean

---
+++ js/mean.js
@@ -0,0 +1,12 @@
+function mean() {
+    sum = 0
+    answer = 0
+	nums = document.getElementById('nums').value.split(',')
+	for (i = 0; i < nums.length; i++) {
+		sum += parseInt(nums[i])
+    }
+
+    answer = sum / nums.length
+    
+	document.getElementById('mean').innerHTML = answer
+}
