6a06612e4e813087fe71203d3a179274f7069741
Nutifafa Attor <attorfafa@gmail.com>
2022-01-16 13:30:09 +0000

    median

---
+++ js/median.js
@@ -0,0 +1,40 @@
+function median() {
+	//store user input
+	var dataSet = []
+
+	// extracting user input
+	var nums = document.getElementById('nums').value.split(',')
+
+	dataSet.push(nums)
+
+	val = ''
+
+	// appending new numbers
+	for (i = 0; i < dataSet.length; i++) {
+		val += dataSet[i]
+	}
+
+	const dataString = dataSet[0]
+	// console.log(dataString)
+
+	// changing from string to numbers
+	var dataNumber = dataString.map((i) => Number(i))
+	console.log(dataNumber)
+
+	// arranging in ascending order
+	dataNumber = dataNumber.sort(function (a, b) {
+		return a - b
+	})
+
+	if (dataNumber.length % 2 == 1) {
+		// length is odd
+		console.log(dataNumber[dataNumber.length / 2 - 0.5])
+	} else {
+		console.log(
+			(dataNumber[dataNumber.length / 2] +
+				dataNumber[dataNumber.length / 2 - 1]) / 2
+		)
+	}
+
+	console.log(dataNumber)
+}
