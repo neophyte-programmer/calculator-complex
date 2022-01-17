1345097626061725f8e0378b7059889fe345dbd6
Nutifafa Attor <attorfafa@gmail.com>
2022-01-17 12:15:48 +0000

    permutations

---
+++ js/permutation.js
@@ -0,0 +1,20 @@
+function getPermutation() {
+    
+
+	// extracting user input
+	var nString = document.getElementById('n').value
+	var rString = document.getElementById('r').value
+
+    // casting the input from string to integer
+    var n = Number(nString)
+    var r = Number(rString)
+
+	
+    
+
+
+
+
+    document.getElementById("permutation").innerHTML = answer
+
+}
