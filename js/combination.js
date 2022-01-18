3cf7d6f4e5bc0ba941b3b5440a4bfe40e9810a24
Nutifafa Attor <attorfafa@gmail.com>
2022-01-18 09:28:32 +0000

    permutations

---
+++ js/combination.js
@@ -0,0 +1,25 @@
+function getCombination() {
+	// extracting user input
+	var nString = document.getElementById('n').value
+	var rString = document.getElementById('r').value
+
+	// casting the input from string to integer
+	var n = Number(nString)
+	var r = Number(rString)
+
+	// function to factorial
+	function factorialize(num) {
+		if (num === 0 || num === 1) return 1
+		for (var i = num - 1; i >= 1; i--) {
+			num *= i
+		}
+		return num
+	}
+	
+    var nominator = factorialize(n)
+    var denominator = factorialize(r) * factorialize(n - r)
+    
+    answer = nominator / denominator
+
+	document.getElementById("combination").innerHTML = answer.toFixed(5)
+}
